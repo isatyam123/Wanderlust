5af04248e0270e8516c16661bc82e5a18f823489
Satyam Kumar <satyamkumar0044@gmail.com>
2026-06-20 03:59:34 +0530

    Fix render port

--- app.js
+++ app.js
@@ -102,6 +102,8 @@ app.use((err,req,res,next) => {
     res.status(statusCode).render("error.ejs", { err });
 });
 
-app.listen(8080, () => {
-    console.log(`Server is listening on port 8080`);
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
